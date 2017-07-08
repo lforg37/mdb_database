3bfbdbc82ecaa9f5c06d9859f1004304aca83fa0
Luc Forget <luc.forget@insa-lyon.fr>
2017-07-08 23:27:29 +0200

    Fin

--- src/java/Utils.java
+++ src/java/Utils.java
@@ -45,18 +45,24 @@ public class Utils {
         HashMap<java.lang.String, java.lang.String> filename_id = new HashMap<java.lang.String, java.lang.String>();
         try {
             Utils.print_log("Getting filename and id with sql request");
-            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/orcl", "user_mmdb", "user_mmdb");
-            String query = "SELECT ROWID, FILE_PATH FROM SYS.IMAGES_TABLE";
+            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/orcl", "SYS", "oracle");
+            Utils.print_log("Connection created");
+            String query = "SELECT ROWID, FILE_PATH FROM IMAGES_TABLE";
 
             Statement request = connection.createStatement();
+            Utils.print_log("Blabla");
 
             ResultSet rs = request.executeQuery(query);
+            Utils.print_log("Blibli");
             while(rs.next()) {
                 filename_id.put(rs.getString("FILE_PATH"), rs.getString("ROWID"));
             }
+            Utils.print_log("Blublu");
 
             request.close();
+            Utils.print_log("Blaablaa");
             connection.close();
+            Utils.print_log("Bliiblii");
         } catch (Exception e) {
 	    Utils.print_log("Request id");
             Utils.print_log(e.getMessage());
